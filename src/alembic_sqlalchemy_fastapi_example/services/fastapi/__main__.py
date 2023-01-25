3aa38d3abb160a12e90bd85943309bdcbca434f6
Yui Chun Leung <leo.leung@faculty.ai>
2023-01-25 15:40:08 +0000

    udpate fastapi to use both path and query params

--- src/alembic_sqlalchemy_fastapi_example/services/fastapi/__main__.py
+++ src/alembic_sqlalchemy_fastapi_example/services/fastapi/__main__.py
@@ -1,10 +1,14 @@
-from typing import List, Optional
+from typing import Optional
 
 from fastapi import FastAPI, HTTPException
 from sqlmodel import Session, select
 
 from alembic_sqlalchemy_fastapi_example.definitions.database import DbAccess
-from alembic_sqlalchemy_fastapi_example.definitions.dataclasses import Visitor
+from alembic_sqlalchemy_fastapi_example.definitions.dataclasses import (
+    InternetProtocol,
+    Topic,
+    Visitor,
+)
 
 app = FastAPI()
 engine = DbAccess(
@@ -22,11 +26,49 @@ def root() -> dict:
     return {"msg": "Hello, World!"}
 
 
-@app.get("/visitors/", response_model=List[Visitor])
+@app.get("/visitors/")
 def read_visits_by_visitor_ip(visitor_ip: str, max_results: Optional[int] = 3):
     with Session(engine) as session:
         statement = (
-            select(Visitor).where(Visitor.ip == visitor_ip).limit(max_results)
+            select(
+                InternetProtocol.address,
+                InternetProtocol.country,
+                Topic.name,
+                Topic.version,
+                Visitor.created_at,
+            )
+            .select_from(Visitor)
+            .join(Topic)
+            .join(InternetProtocol)
+            .where(InternetProtocol.address == visitor_ip)
+            .limit(max_results)
+        )
+        visits = session.exec(statement).all()
+        if not visits:
+            raise HTTPException(
+                status_code=404, detail="Visit Records not found."
+            )
+        return visits
+
+
+@app.get("/visitors/{topic}/{version}/")
+def read_visits_by_topic(
+    *, topic: str, version: str, max_results: Optional[int] = 3
+):
+    with Session(engine) as session:
+        statement = (
+            select(
+                InternetProtocol.address,
+                InternetProtocol.country,
+                Topic.name,
+                Topic.version,
+                Visitor.created_at,
+            )
+            .select_from(Topic)
+            .join(Visitor, Visitor.topic_id == Topic.id)
+            .join(InternetProtocol, InternetProtocol.id == Visitor.ip_id)
+            .where((Topic.name == topic) & (Topic.version == version))
+            .limit(max_results)
         )
         visits = session.exec(statement).all()
         if not visits:
